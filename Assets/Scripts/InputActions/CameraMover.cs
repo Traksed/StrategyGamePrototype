6e1da8f6ac955ba022a002db85f58ee87011b340
Traksed <traksed@gmail.com>
2022-10-06 16:51:59 +0300

    Some changes in CameraMover.cs

--- Assets/Scripts/InputActions/CameraMover.cs
+++ Assets/Scripts/InputActions/CameraMover.cs
@@ -7,12 +7,16 @@ namespace InputActions
 {
     public class CameraMover : MonoBehaviour
     {
+        [SerializeField] private float moveSpeed;
+        
         private CameraInput _cameraInput;
+        private Vector2 _direction;
 
         private void Awake()
         {
             _cameraInput = new CameraInput();
-            
+            _cameraInput.Camera.Move.performed += ctx => OnMove();
+
         }
 
         private void OnEnable()
@@ -23,6 +27,11 @@ namespace InputActions
         private void OnDisable()
         {
             _cameraInput.Disable();
+        }
+
+        private void OnMove()
+        {
+            
         }
     }
 }
